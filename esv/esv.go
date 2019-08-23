b0fde74ec5e7902bb89937659270415350892ffa
Chris Burgin <chrisburgin95@gmail.com>
2019-08-23 16:51:42 -0400

    adds function to return verse HTML

--- esv/esv.go
+++ esv/esv.go
@@ -48,3 +48,33 @@ func GetVerse(search string) (string, error) {
 
 	return verse.Passages[0], err
 }
+
+// GetVerseHTML returns the verse HTML
+func GetVerseHTML(search string) (string, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "https://api.esv.org/v3/passage/html/?q="+search, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Add("Authorization", "Token "+os.Getenv("CROSSWAY_API_KEY"))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	verse, err := fetchVerse(body)
+	if err != nil {
+		return "", err
+	}
+
+	return verse.Passages[0], err
+}
